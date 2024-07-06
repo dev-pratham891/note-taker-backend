8cac2bc826d38eb5db540821a9cdd99d0cb6d9f5
Pratham Singh <prathambsingh891@gmail.com>
2024-07-07 00:15:12 +0530

    update index.js

--- src/app.js
+++ src/app.js
@@ -6,15 +6,6 @@ const app = express();
 
 app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN,
-//     credentials: true,
-//   })
-// );
-
-// "use" is a method that allows you to add middleware to the application
-
 app.use(
   express.json({
     limit: "16kb",
@@ -38,5 +29,9 @@ app.use(express.json());
 
 app.use("/api/auth", authRouter);
 app.use("/api/notes", notesRouter);
+
+app.get("/", (req, res) => {
+  res.send(`hello world`);
+});
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,66 @@
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import { app } from "./app.js";
+// import { app } from "./app.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { router as authRouter } from "./routes/auth.route.js";
+import { router as notesRouter } from "./routes/notes.route.js";
 
 dotenv.config({ path: "./.env" });
+
+
+
+const app = express();
+
+
 
 connectDB()
   .then(() => {
     console.log("Connected to MongoDB");
 
-    app.get("/", (req, res) => {
-      res.send(`hello world`);
-    });
-
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running on port ${process.env.PORT || 8000}`);
-    });
   })
   .catch((error) => {
     console.log("MONGODB connection FAILED ", error);
     process.exit(1);
   });
+
+
+
+
+
+app.use(cors());
+
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/notes", notesRouter);
+
+app.get("/", (req, res) => {
+  res.send(`hello world`);
+});
+
+// export { app };
+
+
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8000}`);
+});
